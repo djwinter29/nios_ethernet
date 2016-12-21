13882d26701f470debf51b97162f05ad36217ad7
ji.dong@hotmail.co.uk <ji.dong@hotmail.co.uk>
2016-12-21 16:34:09 +0000

    move tse out of module

--- PerfDev.sdc
+++ PerfDev.sdc
@@ -9,10 +9,15 @@ set_time_format -unit ns -decimal_places 3
 #**************************************************************
 create_clock -name {Ref_Clk}  -period 8.000 -waveform { 0.000 4.000 } [get_ports {Ref_clk}] 
 
+# Marvell Ethernet RX clock from PHY
+create_clock -name rgmii_rx_clk -period 8.000  -waveform { 0.000 4.000 } [get_ports Enet_rx_clk]
+
 #**************************************************************
 # Create Generated Clock
 #**************************************************************
 derive_pll_clocks 
+
+set tx_clk_125 {thePLL|*pll1|clk[1]}
 
 #**************************************************************
 # Set Clock Uncertainty
@@ -24,3 +29,44 @@ derive_clock_uncertainty
 set_input_delay -clock altera_reserved_tck -clock_fall 2 [get_ports altera_reserved_tdi]
 set_input_delay -clock altera_reserved_tck -clock_fall 2 [get_ports altera_reserved_tms]
 set_output_delay -clock altera_reserved_tck -clock_fall 2 [get_ports altera_reserved_tdo]
+
+
+#**************************************************************
+# Output Delay Constraints (Edge Aligned, Same Edge Capture)
+#**************************************************************  
+# max delay = tsu (-0.9) + skew (0.075) 
+# min delay = -th ( 2.7) - skew (0.075) 
+set_output_delay -add_delay -clock [get_clocks $tx_clk_125] -max -0.825 [get_ports {Enet_tx_en Enet_tx_d*}]
+set_output_delay -add_delay -clock [get_clocks $tx_clk_125] -min -2.775 [get_ports {Enet_tx_en Enet_tx_d*}]
+set_output_delay -add_delay -clock [get_clocks $tx_clk_125] -max -clock_fall -0.825 [get_ports {Enet_tx_en Enet_tx_d*}]
+set_output_delay -add_delay -clock [get_clocks $tx_clk_125] -min -clock_fall -2.775 [get_ports {Enet_tx_en Enet_tx_d*}]
+
+#enet_gtx_clk_125
+# On same edge capture DDR interface the paths from RISE to RISE and FALL to FALL are not valid for hold analysis
+set_false_path -setup -rise_from [get_clocks $tx_clk_125] -fall_to [get_clocks $tx_clk_125]
+set_false_path -setup -fall_from [get_clocks $tx_clk_125] -rise_to [get_clocks $tx_clk_125]
+
+#**************************************************************
+# Input Delay Constraints (Center aligned, Same Edge Analysis)
+#**************************************************************
+# max delay = tco (-1.2) + skew (0.075)
+# min delay = tcomin (-2.8) - skew (0.075)
+
+# Constraint the path to the rising edge of the phy clock
+set_input_delay -add_delay -clock rgmii_rx_clk -max -1.125 [get_ports {Enet_rx_en Enet_rx_d*}]
+set_input_delay -add_delay -clock rgmii_rx_clk -min -2.875 [get_ports {Enet_rx_en Enet_rx_d*}]
+
+# Constraint the path to the falling edge of the phy clock
+set_input_delay -add_delay -clock rgmii_rx_clk -max -clock_fall -1.125 [get_ports {Enet_rx_en Enet_rx_d*}]
+set_input_delay -add_delay -clock rgmii_rx_clk -min -clock_fall -2.875 [get_ports {Enet_rx_en Enet_rx_d*}]
+
+
+# On a same edge capture DDR interface the paths from RISE to FALL and
+# from FALL to RISE on not valid for setup analysis
+set_false_path -setup -rise_from [get_clocks rgmii_rx_clk] -fall_to [get_clocks rgmii_rx_clk]
+set_false_path -setup -fall_from [get_clocks rgmii_rx_clk] -rise_to [get_clocks rgmii_rx_clk]
+
+# On a same edge capture DDR interface the paths from RISE to RISE and
+# FALL to FALL are not avlid for hold analysis
+set_false_path -hold -rise_from [get_clocks rgmii_rx_clk] -rise_to [get_clocks rgmii_rx_clk]
+set_false_path -hold -fall_from [get_clocks rgmii_rx_clk] -fall_to [get_clocks rgmii_rx_clk]
--- PerfDev.vhd
+++ PerfDev.vhd
@@ -7,19 +7,83 @@ use core.core;
 entity PerfDev is
     port (
         -- Clock and Reset
-        Ref_clk                         : in  std_logic:= '0'   
+        Ref_clk                         : in  std_logic:= '0';
+        
+        -- ******* Ethernet (MAC to Marvell PHY) *********
+        -- MDIO
+        Enet_mdc        :   out std_logic;
+        Enet_mdio       : inout std_logic;  
+        
+        -- RGMII 
+        Enet_rx_clk     :    in std_logic                               := 'X';
+        Enet_rx_en      :    in std_logic                               := 'X';
+        Enet_rx_d       :    in std_logic_vector(3 downto 0)            := (others => 'X');
+        Enet_gtx_clk    :   out std_logic;
+        Enet_tx_en      :   out std_logic;
+        Enet_tx_d       :   out std_logic_vector(3 downto 0);
+        
+        -- PHY Reset 
+        Enet_reset_n    :   out std_logic
+        -- **************** Ethernet End ******************
+        
         );
 end PerfDev;
 
 
 architecture rtl of PerfDev is
     signal sys_clk          : std_logic;
+    signal clk_125          : std_logic;
+    
+    -- MDIO Temp Signal
+    signal mdio_in_pad      : std_logic;
+    signal mdio_out_pad     : std_logic;
+    signal mdio_oen_pad     : std_logic;
     
 begin
+
+
+    -- ****** MDIO **************
+    mdio_in_pad                     <= Enet_mdio;
+    Enet_mdio                       <= mdio_out_pad when mdio_oen_pad = '0' else 'Z';
+    
+
+
+
     sys_core : core.core
     port map (
-        clk_clk       => sys_clk,       --   clk.clk
-        reset_reset_n => '1'  -- reset.reset_n
+        clk_clk                     => sys_clk,                     --                   clk.clk
+        reset_reset_n               => '1',                          --                 reset.reset_n
+
+        tse_mac_misc_ff_tx_crc_fwd => '0', --                      enet_mac_misc.ff_tx_crc_fwd
+        tse_mac_misc_ff_tx_septy   => open,   --                      .ff_tx_septy
+        tse_mac_misc_tx_ff_uflow   => open,   --                      .tx_ff_uflow
+        tse_mac_misc_ff_tx_a_full  => open,  --                      .ff_tx_a_full
+        tse_mac_misc_ff_tx_a_empty => open, --                      .ff_tx_a_empty
+        tse_mac_misc_rx_err_stat   => open,   --                      .rx_err_stat
+        tse_mac_misc_rx_frm_type   => open,   --                      .rx_frm_type
+        tse_mac_misc_ff_rx_dsav    => open,    --                      .ff_rx_dsav
+        tse_mac_misc_ff_rx_a_full  => open,  --                      .ff_rx_a_full
+        tse_mac_misc_ff_rx_a_empty => open, --                      .ff_rx_a_empty
+        
+        tse_mac_rgmii_rgmii_in     => Enet_rx_d,     --        enet_mac_rgmii.rgmii_in
+        tse_mac_rgmii_rgmii_out    => Enet_tx_d,    --                      .rgmii_out
+        tse_mac_rgmii_rx_control   => Enet_rx_en,   --                      .rx_control
+        tse_mac_rgmii_tx_control   => Enet_tx_en,   --                      .tx_control
+        
+        tse_pcs_mac_rx_clock_clk   => Enet_rx_clk,   -- enet_pcs_mac_rx_clock.clk
+        tse_pcs_mac_tx_clock_clk   => clk_125,   -- enet_pcs_mac_tx_clock.clk
+        
+        tse_mac_status_set_10      => open,      --       enet_mac_status.set_10
+        tse_mac_status_set_1000    => open,    --                      .set_1000
+        tse_mac_status_eth_mode    => open,    --                      .eth_mode
+        tse_mac_status_ena_10      => open,      --                      .ena_10
+        
+        tse_mac_mdio_mdc           => Enet_mdc,           --         tse_mac_mdio.mdc
+        tse_mac_mdio_mdio_in       => mdio_in_pad,       --                     .mdio_in
+        tse_mac_mdio_mdio_out      => mdio_out_pad,      --                     .mdio_out
+        tse_mac_mdio_mdio_oen      => mdio_oen_pad       --                     .mdio_oen
+
+
     );
     
     thePLL : entity work.clock 
@@ -27,7 +91,8 @@ begin
     (
         --areset  => '0',
         inclk0  => Ref_clk,
-        c0      => sys_clk
+        c0      => sys_clk,
+        c1      => clk_125
     );
 
 end rtl;
